04d256caca656dda9f0206f2ba23e08a7cc48705
Re-isNaN <renangusoncabral@gmail.com>
2025-10-30 08:51:37 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Simulador } from './pages/Simulador'
 import { FormularioSimuladorProvider } from './pages/Simulador/contexts/formularioContext'
 import { SimuladorProvider } from './pages/Simulador/contexts/simulacaoContext'
 
+// teste
+
 export function App() {
   return (
     <ThemeProvider theme={temaPadrao}>
